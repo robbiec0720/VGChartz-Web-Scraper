f2946c13d32522ae02788c44f710dc969c97618f
Robbie Clark <robbiec0720@gmail.com>
2023-10-20 09:45:38 -0500

    fixed issue with error code 429

--- scraper.py
+++ scraper.py
@@ -1,26 +1,46 @@
+import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup, element
+from user_agent import generate_user_agent
 
 def get_all_data(filename):
     all_data = []
     game_list = open(filename, 'r')
+    
     for game in game_list:
         all_data += get_game_data(game.strip())
         print(f'{game.strip()} Done')
+            
     game_list.close()
     return all_data
 
 def get_game_data(game_name):
-    # retrieve webpage info
-    url = f"http://www.vgchartz.com/games/games.php?page=1&name={game_name}&order=TotalSales&ownership=Both&showtotalsales=1&shownasales=1&showpalsales=1&showjapansales=1&showothersales=1&showpublisher=0&showdeveloper=0&showreleasedate=0&showlastupdate=0&showvgchartzscore=0&showcriticscore=0&showuserscore=0&showshipped=0&showmultiplat=Yes"
-    search_results = requests.get(url)
+    # link to search results
+    url = f"https://www.vgchartz.com/games/games.php?name={game_name}&keyword=&console="
+    url += "&region=All&developer=&publisher=&goty_year=&genre=&boxart=Both&banner=Both"
+    url += "&ownership=Both&showmultiplat=Yes&results=50&order=TotalSales&showtotalsales=0&showtotalsales=1"
+    url += "&showpublisher=0&showvgchartzscore=0&shownasales=0&shownasales=1&showdeveloper=0"
+    url += "&showcriticscore=0&showpalsales=0&showpalsales=1&showreleasedate=0&showuserscore=0"
+    url += "&showjapansales=0&showjapansales=1&showlastupdate=0&showothersales=0&showothersales=1"
+    url += "&showshipped=0"
+    # headers = {'User-Agent': generate_user_agent(device_type='desktop', os=('mac', 'linux'))}
     
+    # retry until successful
+    while True:
+        # retrieve webpage info
+        search_results = requests.get(url)
+        if search_results.status_code == 429:
+            time.sleep(30)
+            continue  
+        elif search_results.status_code == 200:
+            break
+        
     # navigate to table
     soup = BeautifulSoup(search_results.text, 'lxml')
     table = soup.find('div', {'id': 'generalBody'})
-    rows = table.find_all('tr')[3:-1]
     
+    rows = table.find_all('tr')[3:-1]
     data = [] # stores data
     consoles = ['PC', 'Xbox', 'X360', 'XOne', 'XSX', 'PS2', 'PS3', 'PS4', 'PS5', 'GCN', 'Wii', 'WiiU', 'NS']
     
@@ -41,12 +61,19 @@ def get_game_data(game_name):
     return data
 
 def get_genre(link):
+    # retry until successful
+    while True:
+        # retrieve webpage info
+        game_page = requests.get(link)
+        if game_page.status_code == 429:
+            time.sleep(30)
+            continue
+        elif game_page.status_code == 200:
+            break
+          
     # retrieve webpage info and navigate to game info section
-    game_page = requests.get(link)
     sub_soup = BeautifulSoup(game_page.text, "lxml")
     gamebox = sub_soup.find('div', {'id': 'gameGenInfoBox'})
-    if gamebox is None:
-        return "N/A"
     h2s = gamebox.find_all('h2')
     
     # this info is not tagged so a manual search is necessary
@@ -60,6 +87,7 @@ def get_genre(link):
 
 print('*****Extracting Sales Data*****')
 sales_data = get_all_data('game_list.txt')
+#sales_data = get_game_data('Fallout 3')
 sales_df = pd.DataFrame(sales_data, columns=['Title', 'Genre', 'Console', 'Total Sales', 'NA Sales', 'PAL Sales', 'Japan Sales', 'Other Sales'])
 sales_df.to_csv('sales_data.csv', sep=',', index=False, encoding='utf-8')
 print('******Extraction Finished******')
